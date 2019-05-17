655d4c15d70fa7d08ff9d9e5c5a06d0849d420f4
herb-medicne <49861049+herb-medicne@users.noreply.github.com>
2019-05-17 15:26:04 +0300

    Update meridians.py

--- data process/meridians.py
+++ data process/meridians.py
@@ -12,14 +12,14 @@ import codecs
 from functools import reduce
 
 
-##process_3 : get herb with high quality meridians. delete duplicated. and get herb meridian features
-# see othe .py file
 
-def meridian_feature(pd_meridians, precessed=True, herb_list_need=None):
+def meridian_feature(pd_meridians, processed=True, herb_list_need=None):
+    # when use herb information down load directly from TCMID database, processed is Flase. 
+    # Otherwise, if have chose herbs manually,  processed is True.
     if precessed == False:
         pd_meridians = pd_meridians.loc[:, 'herb-id':'Meridians']
         pd_meridians['Meridians'] = pd_meridians['Meridians'].str.strip()
-        # select only herbs with meridian (we select)
+        # select only herbs with meridian
         pd_meridians_with = pd_meridians[pd_meridians['Meridians'] != 'NA']
         if herb_list_need != None:
             pd_meridians_with = pd_meridians_with.loc[pd_meridians_with['herb-id'].isin(herb_list_need), :]
@@ -29,32 +29,30 @@ def meridian_feature(pd_meridians, precessed=True, herb_list_need=None):
             return 0, 0
     else:
         meridian_result = meridian_generation(pd_meridians, list_meridians)
-    # check wrong
+        
+    # Get all the herbs that do not have any meridian information
     wrong_herb = meridian_result['herb-id'][meridian_result['LUNG'].isna()].tolist()
     if len(wrong_herb) != 0:
         print('these herb meridian is wrong', wrong_herb)
         print('further check delete or change original data')
-        # TODO: check herb wrong
+        # TODO: check wrong herbs
     return meridian_result, wrong_herb
 
-
+# all the possible meridians
 list_meridians = ['LUNG', 'SPLEEN', 'STOMACH', 'BLADDER', 'CARDIOVASCULAR',
                   'GALLBLADDER', 'HEART', 'KIDNEY', 'LARGE INTESTINE', 'LIVER', 'SMALL INTESTINE', 'THREE END']
 
-
-# add new columns about meridians
+# generate meridians matrix. '1' means belonging to specific meridian, '0' means not. 
 def meridian_generation(pd_meridians_with, list_meridians):
     pd_meridians_with = pd.concat([pd_meridians_with, pd.DataFrame(columns=list_meridians)], sort=False)
     pd_meridians_with['herb-id'] = pd_meridians_with['herb-id'].fillna(0.0).astype(int)
-    # pd_meridians_with['herb-id'] = map(lambda x:str(x).upper(),pd_meridians_with['herb-id'])
-    # pd_meridians_with['herb-id'] = pd.Series(pd_meridians_with['herb-id']).str.upper()
-    # fill the meridian with the '0' '1' list based on their belonging to the meridian or not
+    # fill the meridian with the '0' and '1' list based on their belonging to the meridian or not
     pd_meridians_with.loc[:, 'LUNG':'THREE END'] = [meridian_list(i, list_meridians)
                                                     for i in pd_meridians_with['Meridians']]
     return pd_meridians_with
 
 
-# function to turn meridian list to '0,'1' list
+# function to turn meridian cell to a fixed length 0 and 1 list.
 def meridian_list(meridian_one, list_meridians):
     meridian_related = meridian_one.strip().split(',')
     meridian_related = [i.strip() for i in meridian_related]
@@ -64,6 +62,3 @@ def meridian_list(meridian_one, list_meridians):
     else:
         [np.nan] * len(list_meridians)
 
-#exampel:
-#pd_meridians_with = pd.read_excel(open('C:\\Users\\yinyin\\Desktop\\herbpair\\20.repeat all on new data\\Herb_Meridian_Compound_Pair_Final_new_2.xlsx','rb'),sheet_name='Herb_Meridian')
-#herb_meridian_class,wrong_list = meridian_feature(pd_meridians_with,precessed = True,herb_list_need = None)
