a4d0bcf72ed9f62cf6c330ceed864c43cf671d82
herb-medicne <49861049+herb-medicne@users.noreply.github.com>
2019-05-17 15:48:52 +0300

    Update compound_feature.py

--- data process/compound_feature.py
+++ data process/compound_feature.py
@@ -10,36 +10,30 @@ import pandas as pd
 import pickle
 import codecs
 from functools import reduce
-##  deal wirh feature file
 
-
+# if you use the adme from SwissADME data base, some pre-process have to done. 
 def get_related_com_feature(compound_features_all,com_related,need_delete):
+    # only select the compound related
     data_compound_features = compound_features_all[compound_features_all['Ingredientid'].isin(com_related)]
+    # decide how many feature to use. Whether the useless column has been delete manully. If not, select.
     if need_delete == True:
-        # delete columns 'ESOL Class' ,'Ali Class', 'Silicos-IT class'
-        columns_drop = ['ESOL Class', 'Ali Class','Silicos-IT class','iLOGP','XLOGP3','WLOGP','MLOGP','Silicos-IT',
-                        'Log P','iLOGP','XLOGP3','WLOGP','MLOGP','Silicos-IT','Log P',
+        # delete columns useless
+        columns_drop = ['ESOL Class', 'Ali Class','Silicos-IT class','Consensus Log P',
                         'ESOL Solubility (mg/ml)', 'ESOL Solubility (mol/l)',
                         'Ali Solubility (mg/ml)','Ali Solubility (mol/l)',
                         'Silicos-IT Solubility (mg/ml)',
                         'Silicos-IT Solubility (mol/l)']
         data_compound_features_2 = data_compound_features.drop(columns=  columns_drop)
-        # turn factor to '1','0'
+        # make character to number for better modelling
         data_compound_features_2 = data_compound_features_2.replace({'No': '0', 'Yes': '1', 'High': '1', 'Low': '0'})
+        # remove duplicate columns
         data_compound_features_2.drop_duplicates(keep='first')
         return data_compound_features_2
     else:
         data_compound_features.drop_duplicates(keep='first')
         return data_compound_features
 
-def compound_eleven_duplicate_feature(compound_features_all):
-    compound_features_keep = compound_features_all.drop(columns=['iLOGP','XLOGP3','WLOGP','MLOGP','Silicos-IT',
-                                                                   'ESOL Solubility (mg/ml)', 'ESOL Solubility (mol/l)',
-                                                                   'Ali Solubility (mg/ml)','Ali Solubility (mol/l)',
-                                                                   'Silicos-IT Solubility (mg/ml)',
-                                                                   'Silicos-IT Solubility (mol/l)'])
-    return compound_features_keep
-
+# delete seven useless column
 def compound_delete_seven_feature(compound_features_all):
     compound_features_keep = compound_features_all.drop(columns=['Consensus Log P','ESOL Solubility (mg/ml)',
                                                                  'ESOL Solubility (mol/l)','Ali Solubility (mg/ml)',
@@ -47,6 +41,7 @@ def compound_delete_seven_feature(compound_features_all):
                                                                    'Silicos-IT Solubility (mol/l)'])
     return compound_features_keep
 
+# do compound filtering based on solubility and GI absorption as described in our article
 def compound_filter_by_property(compound_features_all):
     compound_features_filtered = compound_features_all[(compound_features_all['ESOL Log S']>= -6)&
                                                        (compound_features_all['Ali Log S']>= -6)&
@@ -54,25 +49,3 @@ def compound_filter_by_property(compound_features_all):
                                                        (compound_features_all['GI absorption'] == 1)]
     return compound_features_filtered
 
-def class_label_ingredient(pd_compound_class_sum_sorted_filter,class_name_one):
-    delete_ingredients = []
-    pd_label = pd.DataFrame(index=pd_compound_class_sum_sorted_filter.index,columns=[class_name_one])
-    class_name_one_new = class_name_one+'_delete'
-    dict_ingredient = pd_compound_class_sum_sorted_filter.groupby('sorted')['Ingredientid'].apply(list).to_dict()
-    dict_ingredient_count = pd_compound_class_sum_sorted_filter.groupby('sorted')['Ingredientid'].apply(list).apply(len).to_dict()
-    sorted_mumber_dupli = [i for i,j in dict_ingredient_count.items() if j >1]
-    for i in sorted_mumber_dupli:
-        ingredients = dict_ingredient[i]
-        class_list = pd_compound_class_sum_sorted_filter.loc[ingredients,class_name_one].clip(upper=1)
-        if len(set(class_list))!=1:
-            delete_ingredients += ingredients
-    pd_label.loc[delete_ingredients,class_name_one] = 'delete'
-    pd_label = pd_label.fillna('not_delete')
-    add_column_list = pd_label[class_name_one].tolist()
-    pd_compound_class_sum_sorted_filter.insert(0,class_name_one_new,add_column_list)
-    return pd_compound_class_sum_sorted_filter
-
-def class_label_ingredient_all(pd_compound_class_sum_sorted_filter,class_names):
-    for class_name_one in class_names:
-        pd_compound_class_sum_sorted_filter = class_label_ingredient(pd_compound_class_sum_sorted_filter, class_name_one)
-    return pd_compound_class_sum_sorted_filter
